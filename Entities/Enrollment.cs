1ae5eab9d28349362f93fca1b0cc78444f911ce8
amiraadawy <miraadawy98@gmail.com>
2025-11-13 00:24:08 +0200

    Add project files.

---
+++ Entities/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayMopIntegration.Entities
+{
+    public class Enrollment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public  Student  student{ get; set; }
+
+        public int CourseId { get; set; }
+        public Course course { get; set; }
+
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+
+        public string Status { get; set; } = "Active";
+
+       public int PaymentId { get; set; }
+        public Payment payment { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+    }
+}
---
+++ Entities/Payment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PayMopIntegration.Entities
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+
+        public string PaymentMethod { get; set; }
+
+        public string TransactionId { get; set; }
+
+        public string Status { get; set; } = "Pending";
+    }
+}
